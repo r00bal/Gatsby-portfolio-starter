46a1e4353b4d51f8e93498e8a0360a68c190dc96
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-01-30 22:51:14 +0100

    create new pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,43 @@
 import React from 'react'
 import About from '../components/about'
+import { StaticQuery, graphql } from 'gatsby'
 
-const AboutPage = () => (
-  <>
-    <About />
-  </>
+export const LISTING_QUERY = graphql`
+  query BlogPostListing {
+    allMarkdownRemark(
+      limit: 10
+      sort: { order: DESC, fields: [frontmatter___date] }
+    ) {
+      edges {
+        node {
+          excerpt
+          frontmatter {
+            title
+            slug
+            date(formatString: "MMM DD, YYYY")
+          }
+        }
+      }
+    }
+  }
+`
+
+const AboutPage = ({ location }) => (
+  <StaticQuery
+    query={graphql`
+      query {
+        markdownRemark(frontmatter: { slug: { eq: "/about" } }) {
+          excerpt
+        }
+      }
+    `}
+    render={data => (
+      <>
+        {console.log(data)}
+        <About />
+      </>
+    )}
+  />
 )
 
 export default AboutPage
