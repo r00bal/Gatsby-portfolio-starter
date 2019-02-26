fd827d7e18ad85e421a7f7d9bd4227082b26a18d
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-02-26 21:58:02 +0100

    install gatsby-remark-relative-images

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,12 @@
 const path = require('path')
 
+// gatsby-node.js
+const { fmImagesToRelative } = require('gatsby-remark-relative-images')
+
+exports.onCreateNode = ({ node }) => {
+  fmImagesToRelative(node)
+}
+
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
