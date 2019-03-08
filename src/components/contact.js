957f5a67808344086b3d9f8563b13d2f2adec4a2
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-03-08 22:24:26 +0100

    add form

--- src/components/contact.js
+++ src/components/contact.js
@@ -20,6 +20,15 @@ const Contact = () => (
             __html: markdownRemark.html,
           }}
         />
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          <input name="name" placeholder="Your Name" type="text" />
+          <button>Send</button>
+        </form>
       </>
     )}
   />
