25b6563e461d460c73f0669b8c6d778e8c07cc6f
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-03-09 11:01:09 +0100

    form up

--- src/components/contact.js
+++ src/components/contact.js
@@ -23,7 +23,6 @@ const Contact = () => (
         <div>
           <form
             name="contact-form"
-<<<<<<< HEAD
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
@@ -32,26 +31,6 @@ const Contact = () => (
             <input name="email" placeholder="name@name.com" type="email" />
             <textarea name="message" />
             <button>Send</button>
-=======
-            method="POST"
-            data-netlify-recaptcha="true"
-            data-netlify="true"
-          >
-            <p>
-              <label>
-                Email: <input type="text" name="name" />
-              </label>
-            </p>
-            <p>
-              <label>
-                Message: <textarea name="message" />
-              </label>
-            </p>
-            <div data-netlify-recaptcha="true" />
-            <p>
-              <button type="submit">Send</button>
-            </p>
->>>>>>> ac7f7902de86af740f23def295b7d4cbc197c81e
           </form>
         </div>
       </>
