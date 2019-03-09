a6eece50bb1dc506a9b49db68bca4cfb074052ef
r00bal <hal9000@MacBook-Pro-r00bal.local>
2019-03-09 13:34:55 +0100

    FORM UP

--- src/components/contact.js
+++ src/components/contact.js
@@ -20,6 +20,33 @@ const Contact = () => (
             __html: markdownRemark.html,
           }}
         />
+        <form
+          name="contact"
+          method="post"
+          data-netlify="true"
+          data-netlify-honeypot="bot-field"
+        >
+          {/* You still need to add the hidden input with the form name to your JSX form */}
+          <input type="hidden" name="form-name" value="contact" />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message" />
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </>
     )}
   />
